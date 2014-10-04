7aad2d723f8804de78ac2e02ed6ed181f47cf3d6
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-10-04 15:29:41 +0100

    Provide a way of listing inputs

--- logpush.go
+++ logpush.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+  "encoding/json"
   "log"
   "net/http"
   "realtime"
@@ -8,13 +9,34 @@ import (
   "github.com/gorilla/mux"
 )
 
+type Inputs struct {
+  Logfile *logfile.LogfileInput
+}
+
+var inputs *Inputs
+
+func ListInputs(rw http.ResponseWriter, req *http.Request) {
+    result, err := json.Marshal(inputs)
+    if err == nil {
+        rw.Write(result)
+    } else {
+        log.Fatal(err)
+    }
+}
+
 func main() {
     broker := realtime.NewServer()
     router := mux.NewRouter()
 
-    router.HandleFunc("/v0/logs/all/realtime", broker.ServeHTTP)
+    inputs = &Inputs{
+        logfile.Init(),
+    }
 
-    go logfile.Register("test.log", broker.Notifier)
+    router.HandleFunc("/v0/logs/all/realtime", broker.ServeHTTP).Methods("GET")
+
+    router.HandleFunc("/v0/logs", ListInputs).Methods("GET")
+
+    go inputs.Logfile.Register("test.log", broker.Notifier)
 
     log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:3000", router))
 }
--- src/inputs/logfile/main.go
+++ src/inputs/logfile/main.go
@@ -7,21 +7,41 @@ import (
   "time"
 )
 
-type LogifileMessage struct {
+type LogfileMessage struct {
   Name string
-  Line string
+  Message string
   TimeRead time.Time
 }
 
-func Register(filename string, output chan []byte) {
+type OpenFileInfo struct {
+  LinesRead int64
+  ErrorsCount int64
+}
+type LogfileInput struct {
+  OpenFiles map[string]*OpenFileInfo
+}
+
+func (this *LogfileInput) Register(filename string, output chan []byte) {
     t, err := tail.TailFile(filename, tail.Config{Follow: true})
     if err == nil {
+        this.OpenFiles[filename] = &OpenFileInfo{0, 0}
         //XXX: broker.Notifier <- t.Lines
         for line := range t.Lines {
-            result, _ := json.Marshal(LogifileMessage{filename, line.Text, line.Time})
-            output <- result
+            this.OpenFiles[filename].LinesRead += 1
+            result, err := json.Marshal(LogfileMessage{filename, line.Text, line.Time})
+            if err == nil {
+                output <- result
+            } else {
+                log.Fatal(err)
+            }
         }
     } else {
         log.Fatal(err)
+    }
+}
+
+func Init() *LogfileInput {
+    return &LogfileInput{
+        make(map[string]*OpenFileInfo),
     }
 }
