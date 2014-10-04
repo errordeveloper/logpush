0d5c64f63372b1046f11a37fea08975b176ca762
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-10-04 13:57:14 +0100

    Add basic routing

--- logpush.go
+++ logpush.go
@@ -5,12 +5,16 @@ import (
   "net/http"
   "realtime"
   "inputs/logfile"
+  "github.com/gorilla/mux"
 )
 
 func main() {
     broker := realtime.NewServer()
+    router := mux.NewRouter()
+
+    router.HandleFunc("/v0/logs/all/realtime", broker.ServeHTTP)
 
     go logfile.Register("test.log", broker.Notifier)
 
-    log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:3000", broker))
+    log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:3000", router))
 }
