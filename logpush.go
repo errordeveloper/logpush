dce3ceed5521836d6e2f6398e781c18f2699e70d
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-10-04 15:30:59 +0100

    go fmt

--- logpush.go
+++ logpush.go
@@ -1,42 +1,42 @@
 package main
 
 import (
-  "encoding/json"
-  "log"
-  "net/http"
-  "realtime"
-  "inputs/logfile"
-  "github.com/gorilla/mux"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"inputs/logfile"
+	"log"
+	"net/http"
+	"realtime"
 )
 
 type Inputs struct {
-  Logfile *logfile.LogfileInput
+	Logfile *logfile.LogfileInput
 }
 
 var inputs *Inputs
 
 func ListInputs(rw http.ResponseWriter, req *http.Request) {
-    result, err := json.Marshal(inputs)
-    if err == nil {
-        rw.Write(result)
-    } else {
-        log.Fatal(err)
-    }
+	result, err := json.Marshal(inputs)
+	if err == nil {
+		rw.Write(result)
+	} else {
+		log.Fatal(err)
+	}
 }
 
 func main() {
-    broker := realtime.NewServer()
-    router := mux.NewRouter()
+	broker := realtime.NewServer()
+	router := mux.NewRouter()
 
-    inputs = &Inputs{
-        logfile.Init(),
-    }
+	inputs = &Inputs{
+		logfile.Init(),
+	}
 
-    router.HandleFunc("/v0/logs/all/realtime", broker.ServeHTTP).Methods("GET")
+	router.HandleFunc("/v0/logs/all/realtime", broker.ServeHTTP).Methods("GET")
 
-    router.HandleFunc("/v0/logs", ListInputs).Methods("GET")
+	router.HandleFunc("/v0/logs", ListInputs).Methods("GET")
 
-    go inputs.Logfile.Register("test.log", broker.Notifier)
+	go inputs.Logfile.Register("test.log", broker.Notifier)
 
-    log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:3000", router))
+	log.Fatal("HTTP server error: ", http.ListenAndServe("localhost:3000", router))
 }
--- src/inputs/logfile/main.go
+++ src/inputs/logfile/main.go
@@ -1,47 +1,47 @@
 package logfile
 
 import (
-  "encoding/json"
-  "github.com/ActiveState/tail"
-  "log"
-  "time"
+	"encoding/json"
+	"github.com/ActiveState/tail"
+	"log"
+	"time"
 )
 
 type LogfileMessage struct {
-  Name string
-  Message string
-  TimeRead time.Time
+	Name     string
+	Message  string
+	TimeRead time.Time
 }
 
 type OpenFileInfo struct {
-  LinesRead int64
-  ErrorsCount int64
+	LinesRead   int64
+	ErrorsCount int64
 }
 type LogfileInput struct {
-  OpenFiles map[string]*OpenFileInfo
+	OpenFiles map[string]*OpenFileInfo
 }
 
 func (this *LogfileInput) Register(filename string, output chan []byte) {
-    t, err := tail.TailFile(filename, tail.Config{Follow: true})
-    if err == nil {
-        this.OpenFiles[filename] = &OpenFileInfo{0, 0}
-        //XXX: broker.Notifier <- t.Lines
-        for line := range t.Lines {
-            this.OpenFiles[filename].LinesRead += 1
-            result, err := json.Marshal(LogfileMessage{filename, line.Text, line.Time})
-            if err == nil {
-                output <- result
-            } else {
-                log.Fatal(err)
-            }
-        }
-    } else {
-        log.Fatal(err)
-    }
+	t, err := tail.TailFile(filename, tail.Config{Follow: true})
+	if err == nil {
+		this.OpenFiles[filename] = &OpenFileInfo{0, 0}
+		//XXX: broker.Notifier <- t.Lines
+		for line := range t.Lines {
+			this.OpenFiles[filename].LinesRead += 1
+			result, err := json.Marshal(LogfileMessage{filename, line.Text, line.Time})
+			if err == nil {
+				output <- result
+			} else {
+				log.Fatal(err)
+			}
+		}
+	} else {
+		log.Fatal(err)
+	}
 }
 
 func Init() *LogfileInput {
-    return &LogfileInput{
-        make(map[string]*OpenFileInfo),
-    }
+	return &LogfileInput{
+		make(map[string]*OpenFileInfo),
+	}
 }
