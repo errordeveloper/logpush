5b9ee0beb815e2825cd4ccb4e0fb351eb346f8f7
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-10-04 14:07:48 +0100

    JSONify

--- src/inputs/logfile/main.go
+++ src/inputs/logfile/main.go
@@ -1,16 +1,25 @@
 package logfile
 
 import (
-  "log"
+  "encoding/json"
   "github.com/ActiveState/tail"
+  "log"
+  "time"
 )
+
+type LogifileMessage struct {
+  Name string
+  Line string
+  TimeRead time.Time
+}
 
 func Register(filename string, output chan []byte) {
     t, err := tail.TailFile(filename, tail.Config{Follow: true})
     if err == nil {
         //XXX: broker.Notifier <- t.Lines
         for line := range t.Lines {
-            output <- []byte(line.Text)
+            result, _ := json.Marshal(LogifileMessage{filename, line.Text, line.Time})
+            output <- result
         }
     } else {
         log.Fatal(err)
