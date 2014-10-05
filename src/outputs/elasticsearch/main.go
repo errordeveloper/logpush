fa2320751910a1d35fa4900b6d9084e4d989a437
Ilya Dmitrichenko <errordeveloper@gmail.com>
2014-10-05 14:47:34 +0100

    The encoders in heka are a bit of an overkill, let us keep it simple for now

--- src/outputs/elasticsearch/main.go
+++ src/outputs/elasticsearch/main.go
@@ -1,14 +1,26 @@
 //package elasticsearch
 package main
 
+//import (
+//	"code.google.com/p/go-uuid/uuid"
+//	"os"
+//	"time"
+//)
 import (
-	"code.google.com/p/go-uuid/uuid"
-	. "github.com/errordeveloper/heka/message"
-	es "github.com/errordeveloper/heka/plugins/elasticsearch"
-	"os"
-	"time"
+	///"bytes"
+	///"errors"
+	"fmt"
+	//. "github.com/mozilla-services/heka/pipeline"
+	///"io/ioutil"
+	"net"
+	///"net/http"
+	///"net/url"
+	///"strings"
+	///"sync"
+	///"time"
 )
 
+/*
 func getTestMessage() *Message {
 	hostname, _ := os.Hostname()
 	field, _ := NewField("foo", "bar", "")
@@ -28,22 +40,339 @@ func getTestMessage() *Message {
 
 	return msg
 }
+*/
 
 func main() {
-	encoder := new(es.ESLogstashV0Encoder)
-	config := encoder.ConfigStruct()
-	config.(*es.ESLogstashV0EncoderConfig).RawBytesFields = []string{
-		"test_raw_field_string",
-		"test_raw_field_bytes",
-		"test_raw_field_string_array",
-		"test_raw_field_bytes_array",
-	}
+	//encoder := new(ESLogstashV0Encoder)
+	//config := encoder.ConfigStruct()
+	//config.(*ESLogstashV0EncoderConfig).RawBytesFields = []string{
+	//	"test_raw_field_string",
+	//	"test_raw_field_bytes",
+	//	"test_raw_field_string_array",
+	//	"test_raw_field_bytes_array",
+	//}
 
-	_ = encoder.Init(config)
-	b, _ := encoder.EncodeMessage(getTestMessage())
+	//_ = encoder.Init(config)
+	//b, _ := encoder.EncodeMessage("Hello!")
 
-	x := es.NewUDPBulkIndexer("localhost:9700", 1200)
+	x := NewUDPBulkIndexer("localhost:9700", 1200)
 	for {
-		x.Index(b)
+		x.Index([]byte(`
+{"index":{"_index":"logstash-2014.10.05","_type":"message"}}
+{"@uuid":"502291ad-3423-40d2-b64e-7f5b8585b9e1","@timestamp":"2014-10-05T13:10:41.509Z","@type":"TEST","@logger":"GoSpec","@severity":6,"@message":"Test Payload","@envversion":"0.8","@pid":63759,"@source_host":"ilya-dmitrichenko.local","@fields":{"foo":"bar","number":64}}`))
 	}
 }
+
+/***** BEGIN LICENSE BLOCK *****
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# The Initial Developer of the Original Code is the Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2013-2014
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s):
+#   Tanguy Leroux (tlrx.dev@gmail.com)
+#   Rob Miller (rmiller@mozilla.com)
+#
+# ***** END LICENSE BLOCK *****/
+
+//package elasticsearch
+
+//import (
+//	"bytes"
+//	"errors"
+//	"fmt"
+//	//. "github.com/mozilla-services/heka/pipeline"
+//	"io/ioutil"
+//	"net"
+//	"net/http"
+//	"net/url"
+//	"strings"
+//	"sync"
+//	"time"
+//)
+
+// Output plugin that index messages to an elasticsearch cluster.
+// Largely based on FileOutput plugin.
+type ElasticSearchOutput struct {
+	flushInterval uint32
+	flushCount    int
+	batchChan     chan []byte
+	backChan      chan []byte
+	// The BulkIndexer used to index documents
+	bulkIndexer BulkIndexer
+
+	// Specify a timeout value in milliseconds for bulk request to complete.
+	// Default is 0 (infinite)
+	http_timeout uint32
+}
+
+/*
+// ConfigStruct for ElasticSearchOutput plugin.
+type ElasticSearchOutputConfig struct {
+	// Interval at which accumulated messages should be bulk indexed to
+	// ElasticSearch, in milliseconds (default 1000, i.e. 1 second).
+	FlushInterval uint32 `toml:"flush_interval"`
+	// Number of messages that triggers a bulk indexation to ElasticSearch
+	// (default to 10)
+	FlushCount int `toml:"flush_count"`
+	// ElasticSearch server address. This address also defines the Bulk
+	// indexing mode. For example, "http://localhost:9200" defines a server
+	// accessible on localhost and the indexing will be done with the HTTP
+	// Bulk API, whereas "udp://192.168.1.14:9700" defines a server accessible
+	// on the local network and the indexing will be done with the UDP Bulk
+	// API. (default to "http://localhost:9200")
+	Server string
+	// Timeout
+	HTTPTimeout uint32 `toml:"http_timeout"`
+}
+
+func (o *ElasticSearchOutput) ConfigStruct() interface{} {
+	return &ElasticSearchOutputConfig{
+		FlushInterval: 1000,
+		FlushCount:    10,
+		Server:        "http://localhost:9200",
+		HTTPTimeout:   0,
+	}
+}
+
+func (o *ElasticSearchOutput) Init(config interface{}) (err error) {
+	conf := config.(*ElasticSearchOutputConfig)
+	o.flushInterval = conf.FlushInterval
+	o.flushCount = conf.FlushCount
+	o.batchChan = make(chan []byte)
+	o.backChan = make(chan []byte, 2)
+	o.http_timeout = conf.HTTPTimeout
+	var serverUrl *url.URL
+	if serverUrl, err = url.Parse(conf.Server); err == nil {
+		switch strings.ToLower(serverUrl.Scheme) {
+		case "http", "https":
+			o.bulkIndexer = NewHttpBulkIndexer(strings.ToLower(serverUrl.Scheme), serverUrl.Host,
+				o.flushCount, o.http_timeout)
+		case "udp":
+			o.bulkIndexer = NewUDPBulkIndexer(serverUrl.Host, o.flushCount)
+		default:
+			err = errors.New("Server URL must specify one of `udp`, `http`, or `https`.")
+		}
+	} else {
+		err = fmt.Errorf("Unable to parse ElasticSearch server URL [%s]: %s", conf.Server, err)
+	}
+	return
+}
+*/
+
+/*
+func (o *ElasticSearchOutput) Run(or OutputRunner, h PluginHelper) (err error) {
+	if or.Encoder() == nil {
+		return errors.New("Encoder must be specified.")
+	}
+	var wg sync.WaitGroup
+	wg.Add(2)
+	go o.receiver(or, &wg)
+	go o.committer(or, &wg)
+	wg.Wait()
+	return
+}
+
+// Runs in a separate goroutine, accepting incoming messages, buffering output
+// data until the ticker triggers the buffered data should be put onto the
+// committer channel.
+func (o *ElasticSearchOutput) receiver(or OutputRunner, wg *sync.WaitGroup) {
+	var (
+		pack     *PipelinePack
+		e        error
+		count    int
+		outBytes []byte
+	)
+	ok := true
+	ticker := time.Tick(time.Duration(o.flushInterval) * time.Millisecond)
+	outBatch := make([]byte, 0, 10000)
+	inChan := or.InChan()
+
+	for ok {
+		select {
+		case pack, ok = <-inChan:
+			if !ok {
+				// Closed inChan => we're shutting down, flush data
+				if len(outBatch) > 0 {
+					o.batchChan <- outBatch
+				}
+				close(o.batchChan)
+				break
+			}
+			outBytes, e = or.Encode(pack)
+			pack.Recycle()
+			if e != nil {
+				or.LogError(e)
+			} else {
+				outBatch = append(outBatch, outBytes...)
+				if count = count + 1; o.bulkIndexer.CheckFlush(count, len(outBatch)) {
+					if len(outBatch) > 0 {
+						// This will block until the other side is ready to accept
+						// this batch, so we can't get too far ahead.
+						o.batchChan <- outBatch
+						outBatch = <-o.backChan
+						count = 0
+					}
+				}
+			}
+		case <-ticker:
+			if len(outBatch) > 0 {
+				// This will block until the other side is ready to accept
+				// this batch, freeing us to start on the next one.
+				o.batchChan <- outBatch
+				outBatch = <-o.backChan
+				count = 0
+			}
+		}
+	}
+	wg.Done()
+}
+
+// Runs in a separate goroutine, waits for buffered data on the committer
+// channel, bulk index it out to the elasticsearch cluster, and puts the now
+// empty buffer on the return channel for reuse.
+func (o *ElasticSearchOutput) committer(or OutputRunner, wg *sync.WaitGroup) {
+	initBatch := make([]byte, 0, 10000)
+	o.backChan <- initBatch
+	var outBatch []byte
+
+	for outBatch = range o.batchChan {
+		if err := o.bulkIndexer.Index(outBatch); err != nil {
+			or.LogError(err)
+		}
+		outBatch = outBatch[:0]
+		o.backChan <- outBatch
+	}
+	wg.Done()
+}
+*/
+
+// A BulkIndexer is used to index documents in ElasticSearch
+type BulkIndexer interface {
+	// Index documents
+	Index(body []byte) error
+	// Check if a flush is needed
+	CheckFlush(count int, length int) bool
+}
+
+/*
+// A HttpBulkIndexer uses the HTTP REST Bulk Api of ElasticSearch
+// in order to index documents
+type HttpBulkIndexer struct {
+	// Protocol (http or https).
+	Protocol string
+	// Host name and port number (default to "localhost:9200").
+	Domain string
+	// Maximum number of documents.
+	MaxCount int
+	// Internal HTTP Client.
+	client *http.Client
+}
+
+func NewHttpBulkIndexer(protocol string, domain string, maxCount int,
+	httpTimeout uint32) *HttpBulkIndexer {
+
+	client := &http.Client{
+		Timeout: time.Duration(httpTimeout) * time.Millisecond,
+	}
+	return &HttpBulkIndexer{
+		Protocol: protocol,
+		Domain:   domain,
+		MaxCount: maxCount,
+		client:   client,
+	}
+}
+
+func (h *HttpBulkIndexer) CheckFlush(count int, length int) bool {
+	if count >= h.MaxCount {
+		return true
+	}
+	return false
+}
+
+func (h *HttpBulkIndexer) Index(body []byte) error {
+	url := fmt.Sprintf("%s://%s%s", h.Protocol, h.Domain, "/_bulk")
+
+	// Creating ElasticSearch Bulk HTTP request
+	request, err := http.NewRequest("POST", url, bytes.NewReader(body))
+	if err != nil {
+		return fmt.Errorf("can't create bulk request: %s", err.Error())
+	}
+	request.Header.Add("Accept", "application/json")
+	response, err := h.client.Do(request)
+	if err != nil {
+		return fmt.Errorf("HTTP request failed: %s", err.Error())
+	}
+	if response != nil {
+		defer response.Body.Close()
+		if response.StatusCode > 304 {
+			return fmt.Errorf("HTTP response error status: %s", response.Status)
+		}
+		if _, err = ioutil.ReadAll(response.Body); err != nil {
+			return fmt.Errorf("can't read HTTP response body: %s", err.Error())
+		}
+	}
+	return nil
+}
+*/
+
+// A UDPBulkIndexer uses the Bulk UDP Api of ElasticSearch
+// in order to index documents
+type UDPBulkIndexer struct {
+	// Host name and port number (default to "localhost:9700")
+	Domain string
+	// Maximum number of documents
+	MaxCount int
+	// Max. length of UDP packets
+	MaxLength int
+	// Internal UDP Address
+	address *net.UDPAddr
+	// Internal UDP Client
+	client *net.UDPConn
+}
+
+func NewUDPBulkIndexer(domain string, maxCount int) *UDPBulkIndexer {
+	return &UDPBulkIndexer{Domain: domain, MaxCount: maxCount, MaxLength: 65000}
+}
+
+func (u *UDPBulkIndexer) CheckFlush(count int, length int) bool {
+	if length >= u.MaxLength {
+		return true
+	} else if count >= u.MaxCount {
+		return true
+	}
+	return false
+}
+
+func (u *UDPBulkIndexer) Index(body []byte) error {
+	var err error
+	if u.address == nil {
+		if u.address, err = net.ResolveUDPAddr("udp", u.Domain); err != nil {
+			return fmt.Errorf("Error resolving UDP address [%s]: %s", u.Domain, err)
+		}
+	}
+	if u.client == nil {
+		if u.client, err = net.DialUDP("udp", nil, u.address); err != nil {
+			return fmt.Errorf("Error creating UDP client: %s", err)
+		}
+	}
+	if u.address != nil {
+		if _, err = u.client.Write(body[:]); err != nil {
+			return fmt.Errorf("Error writing data to UDP server: %s", err)
+		}
+	} else {
+		return fmt.Errorf("Error writing data to UDP server, address not found")
+	}
+	return nil
+}
+
+/*
+func init() {
+	RegisterPlugin("ElasticSearchOutput", func() interface{} {
+		return new(ElasticSearchOutput)
+	})
+}
+*/
